789ff27aa22c7d9409f295042d18a10bb40b0464
Nilesh Chavhan <nchavhan544@gmail.com>
2025-11-15 21:18:34 +0530

    new files

--- INarr.cpp
+++ INarr.cpp
@@ -14,5 +14,6 @@ int main(){
     {
         cout<<marks[i]<<endl;
     }
+
     return 0;
 }
---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n = 30;
+    int sum = 0;
+    for (int i = 1; i <=n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            sum += i;
+        
+        }
+        
+    }
+    cout << sum;
+    cout<< endl;
+    return 0;
+    
+}
