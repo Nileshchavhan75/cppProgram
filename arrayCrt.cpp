bff5db05c2e409dd017e255f534bc24879ae35f0
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-03 22:44:31 +0530

    new program

--- arrayCrt.cpp
+++ arrayCrt.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[7] = {10,-21,56,47,-12,-14,-75};
+    int arr[7] = {10,-21,56,47,-12,-14,-77};
     int size = 7;
     int count = 0;
     for(int i = 0; i< size; i++){
