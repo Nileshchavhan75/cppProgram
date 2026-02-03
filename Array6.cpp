d27ac5174c3e13a8d8649ed60d09c2313024675c
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-03 22:41:40 +0530

    new program

--- Array6.cpp
+++ Array6.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1, 2, 1, 32, 1, 1, 1, 4, 5};
+    int arr[] = {1, 2, 1, 32, 1, 1, 1, 5, 5};
     int size = sizeof(arr)/ sizeof(arr[0]);
     int var = 1;
     int count = 0;
